0deabc637e7d35c56bf8304ccc7665d1ad26da0c
student <student@imac-35.local>
2018-12-05 17:05:18 +0800

    Initial Commit

---
+++ gesture_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gesture_2
+//
+//  Created by student on 2018/12/5.
+//  Copyright © 2018年 2016110445. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
